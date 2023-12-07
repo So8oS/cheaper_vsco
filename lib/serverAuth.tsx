c731f5fb56287a4f07a99d462a007dd81a74a85b
So8oS <Ahmadothman033@gmail.com>
2023-12-07 14:43:24 +0400

    sad try to fix

--- lib/serverAuth.tsx
+++ lib/serverAuth.tsx
@@ -1,19 +1,25 @@
-import { NextApiRequest } from "next";
+import { NextApiRequest, NextApiResponse } from "next";
 import { getSession } from "next-auth/react";
 
 import prismadb from "../lib/prismadb";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/pages/api/auth/[...nextauth]";
 
-const serverAuth = async (req: NextApiRequest) => {
+const serverAuth = async (req: NextApiRequest, res: NextApiResponse) => {
   try {
-    const session = await getSession({ req });
+    // const session = await getSession({ req });
+    //@ts-ignore
+    const session2 = await getServerSession(req, res, authOptions);
+    // console.log({ session });
+    console.log({ session2 });
 
-    if (!session?.user?.email) {
+    if (!session2?.user?.email) {
       throw new Error("Not Signed In");
     }
 
     const currentUser = await prismadb.user.findUnique({
       where: {
-        email: session.user.email,
+        email: session2.user.email,
       },
       select: {
         id: true,
@@ -31,7 +37,7 @@ const serverAuth = async (req: NextApiRequest) => {
         },
       },
     });
-
+    console.log(currentUser);
     return currentUser;
   } catch (error: any) {
     console.error("Error in serverAuth:", error.message);
