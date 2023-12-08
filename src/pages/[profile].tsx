e01c1c754e623d6079382c60b13ef538b7ff7a16
So8oS <Ahmadothman033@gmail.com>
2023-12-08 20:27:53 +0400

    modell added to [profile]

--- src/pages/[profile].tsx
+++ src/pages/[profile].tsx
@@ -1,9 +1,10 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable jsx-a11y/alt-text */
 /* eslint-disable @next/next/no-img-element */
-import React from "react";
+import React, { useState } from "react";
 import axios from "axios";
 import { useRouter } from "next/router";
+import { IoIosCloseCircle } from "react-icons/io";
 
 interface ImageData {
   url: string;
@@ -20,6 +21,8 @@ const Profile = () => {
   const router = useRouter();
   const id = router.query.profile;
   const [user, setUser] = React.useState<UserProfile | null>(null); // Provide the type here
+  const [selectedPhoto, setSelectedPhoto] = useState();
+  const [viewPhoto, setViewPhoto] = useState<boolean>(false);
 
   const get = async () => {
     try {
@@ -51,9 +54,37 @@ const Profile = () => {
           <h1 className="text-[#737373]">{user?.bio}</h1>
         </div>
       </div>
+      {viewPhoto && (
+        <div className="bg-slate-300 rounded  fixed  m-auto top-0 bottom-0 right-0 left-0 flex flex-col justify-center items-center  border border-black ">
+          {/* @ts-ignore */}
+          <img src={selectedPhoto.url} className="w-auto p-1  max-h-[85%] " />
+          <div className="flex flex-col justify-center items-center gap-2 w-full  bg-slate-100">
+            <IoIosCloseCircle
+              onClick={() => {
+                setViewPhoto(false);
+              }}
+              className=" cursor-pointer h-6 w-6 m-3 "
+            />
+          </div>
+        </div>
+      )}
 
       <div className="columns-1 md:columns-3 lg:columns-4 lg:columns-5ry px-2 mt-10 ">
-        {user && user?.pics.map((item: ImageData, index: number) => <img key={index} className="w-80 p-2 mb-2" src={item.url} />).reverse()}
+        {user &&
+          user?.pics
+            .map((item: ImageData, index: number) => (
+              <img
+                onClick={() => {
+                  /* @ts-ignore */
+                  setSelectedPhoto(item);
+                  setViewPhoto(true);
+                }}
+                key={index}
+                className="w-80 p-2 mb-2"
+                src={item.url}
+              />
+            ))
+            .reverse()}
       </div>
     </div>
   );
